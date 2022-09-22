43f973c31da9d9b7257370afa6018a8edf122e2e
SwiftLight <andrevpanchenko@gmail.com>
2022-09-22 14:50:04 +0300

    feat: add CRUD functionality

--- nodejs-notes-app/helpers/NoteSchema.js
+++ nodejs-notes-app/helpers/NoteSchema.js
@@ -3,7 +3,15 @@ import mongoose from "mongoose";
 const NoteSchema = new mongoose.Schema({
     title: {type: String, required: true},
     category: {type: String, required: true},
-    description: {type: String, required: true}
+    description: {type: String, required: true},
+    createdAt: {
+        type: String,
+        default: new Date().toJSON().slice(0,10),
+    },
+    isArchived: {
+        type: Boolean,
+        default: false
+    }
 });
 
 export default mongoose.model("NoteSchema", NoteSchema);
--- nodejs-notes-app/routes/router.js
+++ nodejs-notes-app/routes/router.js
@@ -5,10 +5,10 @@ import NoteController from "../NoteController.js";
 const router = new Router();
 
 router.post("/notes", NoteController.create);
-router.get("/notes");
-router.get("/notes/:id");
+router.get("/notes", NoteController.getAll);
+router.get("/notes/:id", NoteController.getOne);
 router.get("/notes/stats");
-router.patch("/notes/:id");
-router.delete("/notes/:id");
+router.patch("/notes/:id", NoteController.update);
+router.delete("/notes/:id", NoteController.delete);
 
 export default router;
